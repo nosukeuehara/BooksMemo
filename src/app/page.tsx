b4ed580691b7899b80e59822de5aeb212f9ab6f1
nosuke <ryuunosuke06160616@gmail.com>
2025-03-09 22:33:12 +0900

    トップページ編集

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { BookCard, BookHeader } from "@/components/bookCard/BookCard";
 import BookGallery from "@/components/bookGallery/BookGallery";
 import mockBooks from "@/mock/libraryMgMock";
+import { BookDrawer } from "@/service/bookDrawer/BookDrawer";
 import { BookInfo } from "@/types/types";
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
       <BookHeader />
       <BookGallery>
         {books.map((book) => (
-          <BookCard key={book.id} book={book} />
+          <BookDrawer key={book.id} book={book}>
+            <BookCard key={book.id} book={book} />
+          </BookDrawer>
         ))}
       </BookGallery>
     </div>
