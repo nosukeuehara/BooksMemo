d18c5fc9032ee9563882769175f03d6f7b558e57
nosukeuehara <ryuunosuke06160616@gmail.com>
2025-05-24 16:16:25 +0900

    ルート設定修正

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -35,8 +35,8 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/error') &&
-    !request.nextUrl.pathname.startsWith('/maintenance') &&
-    !request.nextUrl.pathname.startsWith('/privacy') &&
+    !request.nextUrl.pathname.startsWith('/maintenance.html') &&
+    !request.nextUrl.pathname.startsWith('/privacy.html') &&
     !request.nextUrl.pathname.startsWith('/api/auth')
   ) {
     const url = request.nextUrl.clone()
